d91da5ba6136db31718300956e626ee9ab755fd6
NOPROD <contact@boucham-amine.fr>
2020-09-08 08:39:08 +0200

    :recycle: :wrench: :alembic: : .js to .ts / css to scss

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const withSass = require("@zeit/next-sass");
+module.exports = withSass({
+  webpack(config, options) {
+    const rules = [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "postcss-loader",
+          },
+          { loader: "sass-loader" },
+        ],
+      },
+    ];
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: [...config.module.rules, ...rules],
+      },
+    };
+  },
+});
